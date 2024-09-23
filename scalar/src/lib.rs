331983e3dc76bbf9c875feecd0097024a45d6953
8lkc <papebilalysow@gmail.com>
2024-09-23 13:33:59 +0000

    🧷 01 - [ VARIABLES ]

---
+++ scalar/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn sum(a: u8, b: u8) -> u8 {a + b}
+pub fn diff(a: i16, b: i16) -> i16 {a - b}
+pub fn pro(a: i8, b: i8) -> i8 {a * b}
+pub fn quo(a: f32, b: f32) -> f32 {if b != 0.0 {a / b} else {0.0}}
+pub fn rem(a: f32, b: f32) -> f32 {if b != 0.0 {a % b} else {0.0}}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let sum_result = sum(234, 2); assert_eq!(sum_result, 236);
+        let diff_result = diff(234, 2); assert_eq!(diff_result, 232);
+        let pro_result = pro(23, 2); assert_eq!(pro_result, 46);
+        let quo_result = quo(22.0, 2.0); assert_eq!(quo_result, 11.0);
+        let rem_result = rem(22.0, 2.0); assert_eq!(rem_result, 0.0);
+    }
+}
