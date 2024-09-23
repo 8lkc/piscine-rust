60f5623a90294905b140d4932f0a080cd0c3b724
8lkc <papebilalysow@gmail.com>
2024-09-23 15:29:00 +0000

    🧷 04 - [ VARIABLES ]

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -7,10 +7,8 @@ fn main() -> io::Result<()> {
         let mut input = String::new();
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
         io::stdin().read_line(&mut input)?;
-        if input.trim() != "The letter e" {
-            number_of_trials += 1;
-            println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
-            println!("The letter e\nNumber of trials: {}", number_of_trials); break;
+        if input.trim() == "The letter e" {
+            println!("Number of trials: {}", number_of_trials); break;
         }
     }
     Ok(())
