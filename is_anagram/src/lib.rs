b213848d4e30d243bfda8f7a8033febc541b874c
8lkc <papebilalysow@gmail.com>
2024-09-28 04:27:03 +0000

    🧷 13 - [ DATA ]

---
+++ is_anagram/src/lib.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+pub fn is_anagram(s1: &str, s2: &str) -> bool {
+    if s1.len() != s2.len() {return false;}
+    let mut count_s1 = HashMap::new();
+    let mut count_s2 = HashMap::new();
+    for char in s1.chars() {*count_s1.entry(char).or_insert(0) += 1;}
+    for char in s2.chars() {*count_s2.entry(char).or_insert(0) += 1;}
+    count_s1 == count_s2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let s1 = "listen";
+        let s2 = "silent";
+        assert_eq!(is_anagram(s1, s2), true);
+    }
+}
