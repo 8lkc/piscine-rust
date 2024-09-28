01a4697fd39e18f82bbc43d1c29b49b1b10104cf
8lkc <papebilalysow@gmail.com>
2024-09-28 04:18:41 +0000

    🧷 11 - [ DATA ]

---
+++ hashing/src/lib.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+pub fn mean(list: &Vec<i32>) -> f64 {
+    if list.is_empty() {return 0.0;}
+    let sum: i32 = list.iter().sum();
+    sum as f64 / list.len() as f64
+}
+
+pub fn median(list: &Vec<i32>) -> i32 {
+    let mut sorted_list = list.clone();
+    sorted_list.sort();
+    let len = sorted_list.len();
+    if len % 2 == 0 {(sorted_list[len / 2 - 1] + sorted_list[len / 2]) / 2}
+    else {sorted_list[len / 2]}
+}
+
+pub fn mode(list: &Vec<i32>) -> i32 {
+    let mut frequency_map = HashMap::new();
+    for &value in list {*frequency_map.entry(value).or_insert(0) += 1;}
+    frequency_map.into_iter().max_by_key(|&(_, count)| count).map_or(0, |(mode_value, _)| mode_value)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let v = vec![4, 7, 5, 2, 5, 1, 3];
+        assert_eq!(mean(&v), 3.857142857142857);
+        assert_eq!(median(&v), 4);
+        assert_eq!(mode(&v), 5);
+    }
+}
