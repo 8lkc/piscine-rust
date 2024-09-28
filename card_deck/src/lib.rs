65e8d787519bc21cf79268ebfcf849edec1effdd
8lkc <papebilalysow@gmail.com>
2024-09-28 01:47:04 +0000

    🧷 03 - [ DATA ]

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use rand::Rng;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, PartialEq)]
+pub enum Rank {Ace, Number(u8), Jack, Queen, King}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Rank {
+    pub fn random() -> Rank {
+        let index = rand::thread_rng().gen_range(1..13);
+        Rank::translate(index)
     }
+
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            2..=10 => Rank::Number(value),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            _ => panic!(""),
+        }
+	}
 }
+
+#[derive(Debug, PartialEq)]
+pub enum Suit {Heart, Diamond, Spade, Club}
+
+impl Suit {
+    pub fn random() -> Suit {
+        let index = rand::thread_rng().gen_range(1..4);
+        Suit::translate(index)
+    }
+
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => panic!(""),
+        }
+	}
+}
+
+
+#[derive(Debug)]
+pub struct Card {pub suit: Suit, pub rank: Rank}
+pub fn winner_card(card: &Card) -> bool {card.rank == Rank::Ace && card.suit == Suit::Spade}
