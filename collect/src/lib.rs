c4fd4bc926438493571a1d1f261659f78b97ec51
8lkc <papebilalysow@gmail.com>
2024-10-01 13:41:24 +0000

    🧷 12 - [ DATA ]

--- collect/src/lib.rs
+++ collect/src/lib.rs
@@ -6,14 +6,3 @@ pub fn bubble_sort(vec: &mut Vec<i32>) {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
