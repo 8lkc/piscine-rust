ac5b03a8a5e043e9fe665668c78a3896819faa67
8lkc <papebilalysow@gmail.com>
2024-10-01 19:22:24 +0000

    🧷 7 - [ ERRORS ]

--- cipher/src/lib.rs
+++ cipher/src/lib.rs
@@ -1,33 +1,19 @@
 #[derive(Debug, Clone, Eq, PartialEq)]
-pub struct CipherError {
-    pub validation: bool,
-    pub expected: String,
-}
+pub struct CipherError {pub validation: bool, pub expected: String}
 
 impl CipherError {
-    pub fn new(validation: bool, expected: String) -> CipherError {
-        CipherError { validation, expected }
-    }
-}
-
-pub fn atbash_cipher(input: &str) -> String {
-    input.chars().map(|c| {
-        if c.is_ascii_alphabetic() {
-            let a = if c.is_ascii_uppercase() { 'A' } else { 'a' };
-            let z = if c.is_ascii_uppercase() { 'Z' } else { 'z' };
-            let mirrored_char = (z as u8) + (a as u8) - (c as u8);
-            mirrored_char as char
-        } else {
-            c
-        }
-    }).collect()
+    pub fn new(validation: bool, expected: String) -> CipherError {CipherError {validation, expected}}
 }
 
 pub fn cipher(original: &str, ciphered: &str) -> Option<Result<bool, CipherError>> {
-    let expected = atbash_cipher(original);
-    if expected == ciphered {
-        Some(Ok(true))
-    } else {
-        Some(Err(CipherError::new(false, expected)))
+    if original.is_empty() {return None;}
+    else {
+        let expected_cipher: String = original.chars().map(|c| {
+            if c.is_ascii_uppercase() {(b'Z' - (c as u8 - b'A')) as char}
+            else if c.is_ascii_lowercase() {(b'z' - (c as u8 - b'a')) as char}
+            else {c}
+        }).collect();
+        if expected_cipher == ciphered {Some(Ok(true))}
+        else {Some(Err(CipherError::new(false, expected_cipher)))}
     }
 }
