2035e3e0f1b7c12ec01df934fcb33dcd35ee94ee
8lkc <papebilalysow@gmail.com>
2024-10-01 14:25:36 +0000

    🧷 1 - [ ERRORS ]

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -3,8 +3,8 @@ pub enum Security {Unknown, High, Medium, Low, BlockServer}
 pub fn fetch_data(server: Result<String, String>, security_level: Security) -> String {
     match security_level {
         Security::Unknown => {server.expect("")}
-        Security::High => {server.unwrap_or("ERROR: program stops".to_string())}
-        Security::Medium => {server.unwrap_or("WARNING: check the server".to_string())}
+        Security::High => {server.unwrap_or_else(|err| panic!("ERROR: program stops {}", err))}
+        Security::Medium => {server.unwrap_or_else(|_| "WARNING: check the server".to_string())}
         Security::Low => {server.unwrap_or_else(|err| format!("Not found: {}", err))}
         Security::BlockServer => {
             match server {
