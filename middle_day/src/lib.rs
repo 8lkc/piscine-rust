ade7bc10079f692401ed31748d500dcd25d2dad2
8lkc <papebilalysow@gmail.com>
2024-10-02 12:01:08 +0000

    🧷 1 - [ CREATES ]

---
+++ middle_day/src/lib.rs
@@ -0,0 +1,8 @@
+use chrono::{Datelike, NaiveDate};
+
+fn is_leap_year(year: i32) -> bool {year % 4 == 0 && year % 100 != 0 || year % 400 == 0}
+
+pub fn middle_day(year: i32) -> Option<chrono::Weekday> {
+    if is_leap_year(year) {return None;}
+    Some(NaiveDate::from_yo_opt(year, 183)?.weekday())
+}
---
+++ middle_day/src/main.rs
@@ -0,0 +1,2 @@
+use middle_day::middle_day;
+fn main() {println!("{:?}", middle_day(1722))}
