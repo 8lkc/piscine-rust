6ba608f17745a6ac426654c7d5ca2aa6fa02f1c5
8lkc <papebilalysow@gmail.com>
2024-10-03 13:38:42 +0000

    🧷 03 - [ PATTERNS ]

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,4 +1,4 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
-    for i in 0..=array.len() {if array[i] == key {return Some(i);}}
+    for i in 0..=array.len()-1 {if array[i] == key {return Some(i);}}
     None
 }
